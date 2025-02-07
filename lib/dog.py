07ea621745fd738d150124e920d31f05348796cd
Daniella Rono <daniellarono@gmail.com>
2025-02-07 15:51:28 +0300

    all tests are passing

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +11,17 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        # Validate and set the name
+        if name is not None:
+            if not isinstance(name, str) or not (1 <= len(name) <= 25):
+                print("Name must be string between 1 and 25 characters.")
+                return
+            self.name = name
+
+        # Validate and set the breed
+        if breed is not None:
+            if breed not in APPROVED_BREEDS:
+                print("Breed must be in list of approved breeds.")
+                return
+            self.breed = breed  # Assign the breed
